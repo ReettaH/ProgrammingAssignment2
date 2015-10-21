a0f232cf504d79748538a2648da3eaced200ce39
ReettaH <reetta.holmila@gmail.com>
2015-10-21 10:29:25 -0400

    Notes added

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions create an inverse matrix and store it to the cache.
 
-## Write a short comment describing this function
+
+## The first function (makeCacheMatrix()) will store an inverse matrix in the
+## cache.
 
 makeCacheMatrix <- function(x = matrix()) {
 
-        im <- NULL
-        set <- function(y) {
-                x <<- y
-                im <<- NULL
+        inverse_matrix <- NULL   #sets the value for inverse matrix NULL locally
+        set <- function(y) {     #sets global values for x (the matrix) 
+                x <<- y          #and inverse_matrix (NULL)
+                inverse_matrix <<- NULL
         }
-        get <- function() x
-        set_inverse <- function(solve) im <<- solve
-        get_inverse <- function() im
-        list(set = set, get = get,
+        get <- function() x      # returns the x (the matrix to be inverted)
+        set_inverse <- function(solve) inverse_matrix <<- solve 
+        #sets the inverted matrix to the inverse_matrix globally       
+        get_inverse <- function() inverse_matrix #returns the inverse_matrix
+        list(set = set, get = get,  #makes a list of functions "makeCacheMatrix"
              set_inverse = set_inverse,
              get_inverse = get_inverse)
 }
 
-## Write a short comment describing this function
+## This function will check if there is already cached inverted matrix for the
+## matrix x, if yes, it returns it, if not it will make the inverted matrix and
+## store it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-                im <- x$get_inverse()
-                if(!is.null(im)) {
+        
+                inverse_matrix <- x$get_inverse() 
+                #sets the inverse_matrix same as in cache.
+                
+                if(!is.null(inverse_matrix)) {   
                         message("getting cached data")
-                        return(im)
+                        return(inverse_matrix)
                 }
-                data <- x$get()
-                im <- solve(data, ...)
-                x$set_inverse(im)
-                im
+                #This if function checks if there is something in the cache, and 
+                #if it's not empty (!is.null), return the value
+                
+                data <- x$get()  #sets the data as the matrix to be inverted
+                inverse_matrix <- solve(data, ...) 
+                #sets the inverse_matrix as inverse of matrix "data"
+                x$set_inverse(inverse_matrix)
+                #sets the inverse_matrix globally as the inverse of matrix x
+                inverse_matrix #return the inverse matrix
 }
